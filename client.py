3828af5350fdfd10ccc779e7188709edfb2829f3
Text-remist <50calibursword@gmail.com>
2024-06-01 21:30:49 -0400

    Update client.py

--- client.py
+++ client.py
@@ -13,13 +13,42 @@ def redrawWindow(win, player, other_players):
     pygame.display.update()
 
 def main():
+    server_number = 0
     n = Network()
-    p = n.getP()
-    if not p:
-        pygame.quit()
-        print(f"Unable to connect to the server -{n.server_ip()}-")
-        return
+    servers = []
+    for server in n.server_list:
+        addr = (server, n.port)
 
+        connection_result = n.server_list_connect(addr)
+        print(f"{server_number}. {server} | Connection result: {connection_result}")
+        server_number+=1
+    while True:
+        try:
+            # Prompt the user to choose a server
+            server_choice = int(input("Which Server? "))
+        except ValueError:
+            print("Not an integer. Please enter a valid server choice.")
+            continue
+
+        # Check if the server choice is within the valid range
+        if 0 <= server_choice < len(n.server_list):
+            # Update the server IP address based on the user's choice
+            n.server = n.server_list[server_choice]
+            print(f"Connecting to {n.server}:{n.port}...")
+
+            # Attempt to get the player object from the selected server
+            p = n.getP()
+
+            # Check if the player object is retrieved successfully
+            if p:
+                print("Connected to the server successfully.")
+                break  # Exit the loop if the connection is successful
+            else:
+                pygame.quit()
+                print(f"Unable to connect to the server - {n.server}:{n.port}")
+                continue  # Continue to prompt the user for a server choice
+        else:
+            print("Invalid server choice. Please enter a valid server index.")
 
     # Constants for the window dimensions
     width = 500
