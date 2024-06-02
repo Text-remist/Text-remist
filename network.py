63c12b9c55b0314a0fd72426995ce546f9044cc4
Text-remist <50calibursword@gmail.com>
2024-06-01 21:31:00 -0400

    Update network.py

--- network.py
+++ network.py
@@ -1,10 +1,10 @@
 import socket
 import pickle
-
 class Network:
     def __init__(self):
+        self.server_list = ["10.0.0.154", "10.0.0.255", "10.0.0.189"]
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "10.0.0.154"  # Server IP
+        self.server = "10.0.0.154" # Server IP
         self.port = 5555
         self.addr = (self.server, self.port)
         self.p = self.connect()
@@ -12,6 +12,15 @@ class Network:
         return self.server
     def getP(self):
         return self.p
+
+    def server_list_connect(self, addr):
+        try:
+            test_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            test_socket.connect(addr)
+            test_socket.close()  # Close the test socket immediately after connection
+            return True  # Connection successful
+        except Exception as e:
+            return False  # Connection failed
 
     def connect(self):
         try:
@@ -23,6 +32,6 @@ class Network:
     def send(self, data):
         try:
             self.client.send(pickle.dumps(data))
-            return pickle.loads(self.client.recv(4096))  # Increased buffer size to handle more data
+            return pickle.loads(self.client.recv(2048))  # Increased buffer size to handle more data
         except socket.error as e:
             print(e)
